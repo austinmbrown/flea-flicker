5aa3bd532b2eba4169e3c20dcb7fc180fff942ac
Austin Brown <austinmbrown@gmail.com>
2017-09-15 19:57:07 -0400

    filter games index by week

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,13 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    if params[:week]
+      @week = params[:week]
+      @games = Game.where(week: params[:week])
+    else
+      @week = current_week
+      @games = Game.where(week: @week)
+    end
   end
 
   # GET /games/1
@@ -18,5 +24,14 @@ class GamesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_game
       @game = Game.find(params[:id])
+    end
+
+    def current_week
+      today = Date.today
+      last_day = Date.new(2018, 2, 22)
+      first_day = Date.new(2017, 8, 22)
+
+      return if today > last_day || today < first_day
+      ((today - first_day)).to_i/7-1
     end
 end
