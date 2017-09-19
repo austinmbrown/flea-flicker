e6c434beb55651a5d25f988c07cf91de47cb77d1
Austin Brown <austinmbrown@gmail.com>
2017-09-19 19:24:35 -0400

    set wins losses and ties to 0 and clean up template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918020203) do
+ActiveRecord::Schema.define(version: 20170919230229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,11 +61,11 @@ ActiveRecord::Schema.define(version: 20170918020203) do
     t.string   "name"
     t.string   "stattleship_id"
     t.string   "logo_url"
-    t.integer  "wins"
-    t.integer  "losses"
-    t.integer  "ties"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "wins",           default: 0
+    t.integer  "losses",         default: 0
+    t.integer  "ties",           default: 0
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
