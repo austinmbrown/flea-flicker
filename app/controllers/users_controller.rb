1aa8da363183d84ca031f38228b5941d0d3b750a
Austin Brown <austinmbrown@gmail.com>
2017-09-21 17:19:07 -0400

    evaluate picks on users index too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    Pick.unevaluated.each(&:evaluate)
     @users = User.all.sort_by{|u| u.picks.correct.count }.reverse
   end
 end
